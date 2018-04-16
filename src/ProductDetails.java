571b7db16b3c794a32adade4d96a7b416d00ebc2
Rares Urs <urs_rares96@yahoo.com>
2018-04-16 23:33:59 +0100

    Add functionality to get kcal for each product

---
+++ src/ProductDetails.java
@@ -0,0 +1,39 @@
+public class ProductDetails {
+    String description;
+    String title;
+    int unitPrice;
+    String kCalories;
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(int unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public String getkCalories() {
+        return kCalories;
+    }
+
+    public void setkCalories(String kCalories) {
+        this.kCalories = kCalories;
+    }
+}
--- src/ScraperServiceImplementation.java
+++ src/ScraperServiceImplementation.java
@@ -11,45 +11,70 @@ public class ScraperServiceImplementation implements ScraperServiceInterface {
     @Override
     public String scrap(String url) {
         Scraper scraper = new Scraper(url);
+        ProductDetails productDetails = new ProductDetails();
+
         try {
             UserAgent userAgent = new UserAgent();
             userAgent.visit(scraper.getUrlToScrap());
-            Elements items = userAgent.doc.findEach("<li class=\"gridItem\"");
+            Elements items = userAgent.doc.findFirst("<ul class=\"productLister gridView\"").findEach("<li class=\"gridItem\"");
+
             for (Element item : items) {
+                try {
+                    String title = item
+                            .findFirst("<div class=\"productInfo\"")
+                            .findFirst("<div class=\"productNameAndPromotions\"")
+                            .findFirst("<h3>")
+                            .findFirst("<a href")
+                            .getText();
 
-                String title = item
-                        .findFirst("<div class=\"productInfo\"")
-                        .findFirst("<div class=\"productNameAndPromotions\"")
-                        .findFirst("<h3>")
-                        .findFirst("<a href")
-                        .getText();
+                    String price = item
+                            .findFirst("<div class=\"addToTrolleytabBox\"")
+                            .findFirst("<div class=\"addToTrolleytabContainer addItemBorderTop\">\n")
+                            .findFirst("<div class=\"pricingAndTrolleyOptions\">\n")
+                            .findFirst("<div class=")
+                            .findFirst("<div class=\"pricing\">\n")
+                            .findFirst("<p class=\"pricePerUnit\">\n")
+                            .getText();
 
-                String urlToVisit = item
-                        .findFirst("<div class=\"productInfo\"")
-                        .findFirst("<div class=\"productNameAndPromotions\"")
-                        .findFirst("<h3>")
-                        .findFirst("<a href").outerHTML();
 
-                String price = item
-                        .findFirst("<div class=\"addToTrolleytabBox\"")
-                        .findFirst("<div class=\"addToTrolleytabContainer addItemBorderTop\">\n")
-                        .findFirst("<div class=\"pricingAndTrolleyOptions\">\n")
-                        .findFirst("<div class=")
-                        .findFirst("<div class=\"pricing\">\n")
-                        .findFirst("<p class=\"pricePerUnit\">\n")
-                        .getText();
+                    String urlToVisit = item
+                            .findFirst("<div class=\"productInfo\"")
+                            .findFirst("<div class=\"productNameAndPromotions\"")
+                            .findFirst("<h3>")
+                            .findFirst("<a href").outerHTML();
 
-                userAgent.visit(urlStrip(urlToVisit)).findFirst("<div class");
+                    String description = userAgent.
+                            visit(urlStrip(urlToVisit))
+                            .findFirst("<div class=\"section\"")
+                            .findFirst("<p>")
+                            .getText();
+
+                    Elements cal = userAgent
+                            .visit(urlStrip(urlToVisit))
+                            .findFirst("<div class=\"tableWrapper\"").findFirst("<table class=\"nutritionTable\"").findEach("<tr class=\"tableRow0\"");
+
+                    for (Element kcal : cal) {
+                        String calDescription = kcal.findFirst("<td").getText();
+
+                        if (calDescription.contains("kcal")) {
+                            System.out.println(calDescription); // Obiectu
+                        }
+                    }
+
+                    //System.out.println(calories.trim());
+                } catch (JauntException e) {
+                    if (e.getMessage().contains("not found: <div class=\"tableWrapper\"")) {
+                        productDetails.setkCalories("");
+                    } else {
+                        System.err.println("There was an error: " + e);
+                    }
+                }
             }
 
         } catch (JauntException e) {
-            System.err.println(e);
+            System.err.println("There was an error: " + e);
         }
         return "Hello";
-    }
-
-    private String removeWhiteSpaces(String string) {
-        return string.trim().replaceAll("\\s+", " ");
     }
 
     private String urlStrip(String url) {
